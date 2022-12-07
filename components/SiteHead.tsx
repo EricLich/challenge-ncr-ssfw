02161c624e8c12b7519ae607c1afdb9771071f6a
Eric Lich <eric.lichtensztein@aleia.io>
2022-12-07 14:17:12 -0300

    initial commit

---
+++ components/SiteHead.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import React from "react";
+
+const SiteHead = () => {
+  return (
+    <Head>
+      <title>Challenge NCR SSFW</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default SiteHead;
